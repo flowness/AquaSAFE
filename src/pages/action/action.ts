80cc95bafcc1b715859d669dd8ca52205af6eff5
flowness <yocohen@gmail.com>
2018-07-03 12:44:59 +0300

    add not a leak options

--- src/pages/action/action.ts
+++ src/pages/action/action.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-action',
@@ -8,10 +9,34 @@ import { NavController, NavParams } from 'ionic-angular';
 export class ActionPage {
   item: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public alertCtrl: AlertController, public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
     this.item = navParams.get('item');
 
   }
+
+  doConfirm(title) {
+   let alert = this.alertCtrl.create({
+     title: title,
+     message: 'Do you currently use the ' + title + '?',
+     buttons: [
+       {
+         text: 'No',
+         handler: () => {
+           console.log('No clicked');
+         }
+       },
+       {
+         text: 'Yes',
+         handler: () => {
+           console.log('Yes clicked');
+           this.navCtrl.pop();
+         }
+       }
+     ]
+   });
+
+   alert.present();
+ }
 
 }
