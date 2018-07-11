db27799bc6fc72092b0602b162490222142db151
Noam Lessner <no.less@live.com>
2018-07-11 21:44:43 +0300

    fix windows platform camera

--- src/pages/events/isaleak/isaleak.ts
+++ src/pages/events/isaleak/isaleak.ts
@@ -44,7 +44,7 @@ export class IsALeakPage {
 
     const options: CameraOptions = {
        quality: 50,
-       destinationType: this.camera.DestinationType.FILE_URI,
+       destinationType: this.camera.DestinationType.DATA_URL,
        encodingType: this.camera.EncodingType.JPEG,
        mediaType: this.camera.MediaType.PICTURE
      }
