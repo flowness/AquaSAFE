52f9e67f4f1e44840485559c0f00f3958fa20b85
yocohen007 <yocohen@gmail.com>
2018-08-08 12:51:52 +0300

    minor fix

--- src/providers/data-finder.ts
+++ src/providers/data-finder.ts
@@ -45,6 +45,6 @@ export class DataFinder {
       errMsg = error.message ? error.message : error.toString();
     }
     console.error(errMsg);
-    // return Observable.throw(errMsg)
+    return Observable.throw(errMsg)
   }
 }
